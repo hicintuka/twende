ec9401a4db01a701b2afced2dd4ae86c29514b89
dhicintuka <dhicintuka@asystresources.com>
2025-11-25 10:38:22 +0200

    init

---
+++ src/main/java/com/cloud_box/twende/TwendeApplication.java
@@ -0,0 +1,13 @@
+package com.cloud_box.twende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwendeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud_box/twende/TwendeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_box.twende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
